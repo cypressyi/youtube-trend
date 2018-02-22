94032239aed1cfe5df326d70cf948b663ddacd26
Cypressyi <cypressyi1990@gmail.com>
2018-02-23 01:25:14 +0800

    Fix: Change some syntax.

--- src/components/SelectForm.js
+++ src/components/SelectForm.js
@@ -12,29 +12,30 @@ const SearchForm = ({ onFetchData }) => {
 
   function handleOnchange() {
     onFetchData(selectValue)
-  }  
+  }
 
   return (
     <div>
       <Dropdown
         className="selectform"
-        placeholder='選擇地區'
+        placeholder="選擇地區"
         defaultValue="tw"
         fluid
         search
         selection
         options={countryOptions}
         onChange={(e, { value }) => {
-          selectValue = value; handleOnchange()}
-        }
+          selectValue = value
+          handleOnchange()
+        }}
       />
     </div>
   )
 }
 
-const mapDispatchToProps = (dispatch) =>(
+const mapDispatchToProps = dispatch => {return (
   bindActionCreators({ onFetchData }, dispatch)
-)
+)}
 
 export default connect(null, mapDispatchToProps)(SearchForm)
 
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Item, Label, Transition } from 'semantic-ui-react'
-import formatView from '../common/viewFormat'
 import moment from 'moment'
+import formatView from '../common/viewFormat'
 
 function VideoItem({
   url,
@@ -12,21 +12,21 @@ function VideoItem({
   thumbnails,
   viewCount,
 }) {
-  let image_url = thumbnails.medium.url
-  let timeAgo = moment(publishedAt).fromNow()
+  const imageUrl = thumbnails.medium.url
+  const timeAgo = moment(publishedAt).fromNow()
 
   return (
-    <Transition transitionOnMount={true} animation='fade' duration={800}>
+    <Transition transitionOnMount animation="fade" duration={800}>
       <Item.Group>
         <Item as="a" href={url} target="_BLANK">
-          <Item.Image size="medium" src={image_url} />
+          <Item.Image size="medium" src={imageUrl} />
           <Item.Content>
             <Item.Header target="_BLANK">{title}</Item.Header>
             <Item.Description className="video-description">{description}</Item.Description>
             <Item.Extra>
-            <Label>{channelTitle}</Label>
-            <Label>{formatView(viewCount)} views</Label>
-            <Label>{timeAgo}</Label>
+              <Label>{channelTitle}</Label>
+              <Label>{formatView(viewCount)} views</Label>
+              <Label>{timeAgo}</Label>
             </Item.Extra>
           </Item.Content>
         </Item>
